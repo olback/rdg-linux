92c5ebb43ee62ab3a7683215f800a106137e17d8
Edwin Svensson <git@olback.net>
2017-11-17 14:56:06 +0100

    v1.1

--- src/vars.h
+++ src/vars.h
@@ -11,7 +11,7 @@
 #include <unistd.h>
 #include <gtk/gtk.h>
 
-char title[64] = "rDesktop GUI 1.0";
+char title[64] = "rDesktop GUI 1.1";
 const char rdPath[] = "/usr/bin/rdesktop";
 
 // Global variables bla bla don't care.
