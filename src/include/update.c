37264bb53f836c2244747701f022971daa212fae
Edwin Svensson <git@olback.net>
2018-09-25 17:41:44 +0200

    formatting

--- src/include/update.c
+++ src/include/update.c
@@ -8,7 +8,8 @@
 #include <curl/curl.h>
 #include <libnotify/notify.h>
 
-void runUpdate() {
+void runUpdate()
+{
     struct Version v = projectJSON(NULL);
     char updateCommand[354];
     sprintf(updateCommand, "xdg-open %s", v.update_url);
@@ -21,12 +22,13 @@ void notifyUpdate(struct Version v)
     sprintf(notifyMsg, "%s %1.1f %s", "Update", v.version, "available!");
 
     notify_init(v.name);
-    NotifyNotification *n = notify_notification_new (v.name, notifyMsg, "package-x-generic");
+    NotifyNotification *n = notify_notification_new(v.name, notifyMsg, "package-x-generic");
     notify_notification_add_action(n, v.name, "Update", NOTIFY_ACTION_CALLBACK(runUpdate), NULL, NULL);
     notify_notification_set_hint_string(n, v.name, notifyMsg);
     notify_notification_set_timeout(n, 5000); // 5 seconds
 
-    if (!notify_notification_show(n, NULL)) {
+    if (!notify_notification_show(n, NULL))
+    {
         printf("Failed to show notification\n");
     }
 }
@@ -34,13 +36,14 @@ void notifyUpdate(struct Version v)
 /* function prototypes to define later */
 char *do_web_request(char *url);
 size_t static write_callback_func(void *buffer,
-                        size_t size,
-                        size_t nmemb,
-                        void *userp);
+                                  size_t size,
+                                  size_t nmemb,
+                                  void *userp);
 
 void *checkUpdates()
 {
-    if(dev) {
+    if (dev)
+    {
         printf("%sNot cheking for updates in dev mode!%s\n", KYEL, KNRM);
         pthread_join(t_update, NULL);
         return NULL;
@@ -52,25 +55,27 @@ void *checkUpdates()
 
     content = do_web_request(url);
 
-    if(content != NULL) {
-
+    if (content != NULL)
+    {
         struct Version vl = projectJSON(NULL);
         printf("\nLocal version: %1.1f\n", vl.version);
 
         struct Version vr = projectJSON(content);
         printf("Remote version: %1.1f\n", vr.version);
 
-        if(vl.version >= vr.version) {
+        if (vl.version >= vr.version)
+        {
             printf("%sRunning latest version%s\n", KGRN, KNRM);
-        } else {
+        }
+        else
+        {
             printf("%sUpdate available%s\n", KYEL, KNRM);
             notifyUpdate(vr);
         }
-
-    } else {
-
+    }
+    else
+    {
         printf("%sFailed to check for updates. Check your connection.%s\n", KRED, KNRM);
-
     }
 
     pthread_join(t_update, NULL);
@@ -103,8 +108,6 @@ char *do_web_request(char *url)
     /* passing the pointer to the response as the callback parameter */
     curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, &response);
 
-    long int response_code;
-
     /* perform the request */
     curl_easy_perform(curl_handle);
 
@@ -116,14 +119,14 @@ char *do_web_request(char *url)
 
 /* the function to invoke as the data recieved */
 size_t static write_callback_func(void *buffer,
-                        size_t size,
-                        size_t nmemb,
-                        void *userp)
+                                  size_t size,
+                                  size_t nmemb,
+                                  void *userp)
 {
-    char **response_ptr =  (char**)userp;
+    char **response_ptr = (char **)userp;
 
     /* assuming the response is a string */
-    *response_ptr = strndup(buffer, (size_t)(size *nmemb));
+    *response_ptr = strndup(buffer, (size_t)(size * nmemb));
 
     return 0;
 }
