4803c73bb6eb03a54dd4b119adb77c008415aa68
Edwin Svensson <git@olback.net>
2020-12-15 04:41:38 +0100

    check if CARGO_TARGET_DIR is set

--- install.sh
+++ install.sh
@@ -10,8 +10,11 @@ if [[ $# -eq 1 && $1 == "uninstall" ]]; then
     echo "Uninstalled"
 
 else
-
-    cp target/release/rdg /usr/bin/rdg
+    if [[ -n $CARGO_TARGET_DIR ]]; then
+        cp $CARGO_TARGET_DIR/release/rdg /usr/bin/rdg
+    else
+        cp target/release/rdg /usr/bin/rdg
+    fi
     cp assets/net.olback.rdg.desktop /usr/share/applications/net.olback.rdg.desktop
     cp assets/net.olback.rdg.png /usr/share/icons/hicolor/256x256/apps/net.olback.rdg.png
 
